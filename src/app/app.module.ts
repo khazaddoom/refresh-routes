806ce867925bfbaf5002b2ea0eb91040044dfac8
unknown <31121986g@gmail.com>
2019-06-19 10:24:30 +0530

    refreshing routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+
+const appRoutes: Route[] = [
+  { path: 'admin', component: AdminComponent},
+  { path: 'content', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
